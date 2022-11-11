caf21be463c56a9406410085c0b55b76f4db39b6
OstrichFarmer <joel.stephen56.sj@gmail.com>
2022-11-11 12:55:21 +0100

    created the api base constant

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -15,117 +15,120 @@ bool selectMode = true;
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(150),
-        child: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.only(
-              left: Dimensions.height24,
-              top: Dimensions.height20,
-              right: Dimensions.height24,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Image(
-                  image: const AssetImage('assets/images/ex_logo.png'),
-                  width: Dimensions.height114,
-                  height: Dimensions.height35,
-                ),
-                InkWell(
-                    onTap: () {
-                      setState(() {
-                        selectMode = !selectMode;
-                      });
-                    },
-                    child: selectMode
-                        ? const Icon(
-                            Icons.wb_sunny_outlined,
-                            size: 30,
-                            color: Colors.black,
-                          )
-                        : const Icon(
-                            Icons.light_mode,
-                            size: 30,
-                          )),
-              ],
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(150),
+          child: SafeArea(
+            child: Padding(
+              padding: EdgeInsets.only(
+                left: Dimensions.height24,
+                top: Dimensions.height20,
+                right: Dimensions.height24,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image(
+                    image: const AssetImage('assets/images/ex_logo.png'),
+                    width: Dimensions.height114,
+                    height: Dimensions.height35,
+                  ),
+                  InkWell(
+                      onTap: () {
+                        setState(() {
+                          selectMode = !selectMode;
+                        });
+                      },
+                      child: selectMode
+                          ? const Icon(
+                              Icons.wb_sunny_outlined,
+                              size: 30,
+                              color: Colors.black,
+                            )
+                          : const Icon(
+                              Icons.light_mode,
+                              size: 30,
+                            )),
+                ],
+              ),
             ),
           ),
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.only(
-            left: Dimensions.height24,
-            top: Dimensions.height24,
-            right: Dimensions.height24,
-          ),
-          child: Column(
-            children: [
-              TextFormField(
-                textAlign: TextAlign.center,
-                onChanged: (_) {},
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: AppColors.paleWhite,
-                  hintText: 'Search Country',
-                  hintStyle: GoogleFonts.montserrat(
-                    fontSize: Dimensions.font20,
-                    fontWeight: FontWeight.w500,
-                    color: const Color(0xff667085),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: Dimensions.height24,
+              top: Dimensions.height24,
+              right: Dimensions.height24,
+            ),
+            child: Column(
+              children: [
+                TextFormField(
+                  textAlign: TextAlign.center,
+                  onChanged: (_) {},
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: AppColors.paleWhite,
+                    hintText: 'Search Country',
+                    hintStyle: GoogleFonts.montserrat(
+                      fontSize: Dimensions.font20,
+                      fontWeight: FontWeight.w500,
+                      color: const Color(0xff667085),
+                    ),
+                    prefixIcon: const Icon(
+                      Icons.search,
+                      color: Colors.black54,
+                    ),
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.all(Dimensions.height16),
                   ),
-                  prefixIcon: const Icon(
-                    Icons.search,
-                    color: Colors.black54,
-                  ),
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.all(Dimensions.height16),
                 ),
-              ),
-              SizedBox(
-                height: Dimensions.height16,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ElevatedButton(
-                    onPressed: () => showModalBottomSheet(
-                        isScrollControlled: true,
-                        backgroundColor: Colors.transparent,
-                        context: context,
-                        builder: (context) => languageSheet()),
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.white,
-                        padding: const EdgeInsets.all(10)),
-                    child: Row(
-                      children: const [
-                        Icon(Icons.language),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text('EN')
-                      ],
+                SizedBox(
+                  height: Dimensions.height16,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => showModalBottomSheet(
+                          isScrollControlled: true,
+                          backgroundColor: Colors.transparent,
+                          context: context,
+                          builder: (context) => languageSheet()),
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          padding: const EdgeInsets.all(10)),
+                      child: Row(
+                        children: const [
+                          Icon(Icons.language),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text('EN')
+                        ],
+                      ),
                     ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.white,
-                        padding: const EdgeInsets.all(10)),
-                    child: Row(
-                      children: const [
-                        Icon(Icons.filter_alt_outlined),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text('Filter')
-                      ],
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          padding: const EdgeInsets.all(10)),
+                      child: Row(
+                        children: const [
+                          Icon(Icons.filter_alt_outlined),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text('Filter')
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              )
-            ],
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
